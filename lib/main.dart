4d5250b89551b958c85b6caab17c9caf2b191664
kanta <kanta.ko@ku.th>
2023-03-05 18:38:48 +0700

    fix code for connect firebase and flutter (android and ios)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 // import 'package:my_book/BottomBar.dart';
 // import 'package:my_book/LogInPage.dart';
@@ -6,7 +7,9 @@ import 'package:my_book/Screen/RegisterPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(MyApp());
 }
 
